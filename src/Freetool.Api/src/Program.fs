c5fa7af1cf14ea754b3c562a01cab77be309963b
Chander Ramesh <chander@wonderly.com>
2026-04-25 21:20:16 -0700

    codex/backport error group 20260425 (#52)
    - **Backport exception telemetry defaults**
    - **Format exception telemetry backport**

--- src/Freetool.Api/src/Program.fs
+++ src/Freetool.Api/src/Program.fs
@@ -590,6 +590,8 @@ let main args =
     app.UseStaticFiles(staticFileOptions) |> ignore
 
     // Use DevAuthMiddleware in dev mode; production always authenticates via Google IAP.
+    app.UseMiddleware<ExceptionHandlerMiddleware>() |> ignore
+
     if isDevMode then
         app.UseMiddleware<DevAuthMiddleware>() |> ignore
     else
